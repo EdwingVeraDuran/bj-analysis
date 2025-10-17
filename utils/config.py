f43a03ecc73c65acd183d16a8512e80b75532c98
Edwing Vera <edwingveraduran@gmail.com>
2025-10-17 12:21:37 -0500

    :heavy_plus_sign: Add app config for simulations.

---
+++ utils/config.py
@@ -0,0 +1,17 @@
+CONFIG = {
+    "decks": 6,
+    "starting_balance": 1000,
+    "base_bet": 10,
+    "penetration": 0.75,
+    "hit_soft_17": False,
+    "blackjack_payout": 1.5,
+    "bet_table": [
+        (-999, 1),
+        (1, 2),
+        (3, 4),
+        (5, 8),
+    ],
+    "simulations": 1000,
+    "csv_path": "data/result.csv",
+    "rng_seed": None,
+}
